7be8107da8143b5dd0bc7471bb37c10d92e369cc
KIPROTYCH <kiprotych@gmail.com>
2023-07-23 03:33:16 +0300

    Modified Transaction.js

--- React-App/flatiron-bank/src/Transactions.js
+++ React-App/flatiron-bank/src/Transactions.js
@@ -6,13 +6,122 @@ function Transactions() {
   const [sortType, setSortType] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8001/transactions")
-      .then((response) => response.json())
-      .then((data) => setTransactions(data));
+    // Replace the fetch call with setting the sample data directly
+    const sampleData = {
+      transactions: [
+        {
+          id: 1,
+          date: "2019-12-01",
+          description: "Paycheck from Bob's Burgers",
+          category: "Income",
+          amount: 1000,
+        },
+        {
+          id: 2,
+          date: "2019-12-01",
+          description: "South by Southwest Quinoa Bowl at Fresh & Co",
+          category: "Food",
+          amount: -10.55,
+        },
+        {
+          id: 3,
+          date: "2019-12-02",
+          description: "South by Southwest Quinoa Bowl at Fresh & Co",
+          category: "Food",
+          amount: -10.55,
+        },
+        {
+          id: 4,
+          date: "2019-12-04",
+          description: "Sunglasses, Urban Outfitters",
+          category: "Fashion",
+          amount: -24.99,
+        },
+        {
+          id: 5,
+          date: "2019-12-06",
+          description: "Venmo, Alice Pays you for Burrito",
+          category: "Food",
+          amount: 8.75,
+        },
+        {
+          id: 6,
+          date: "2019-12-06",
+          description: "Chipotle",
+          category: "Food",
+          amount: -17.59,
+        },
+        {
+          id: 7,
+          date: "2019-12-07",
+          description: "Birthday Check from Grandma",
+          category: "Gift",
+          amount: 50,
+        },
+        {
+          id: 8,
+          date: "2019-12-09",
+          description: "Lyft Ride",
+          category: "Transportation",
+          amount: -13.25,
+        },
+        {
+          id: 9,
+          date: "2019-12-11",
+          description: "Paycheck from Bob's Burgers",
+          category: "Income",
+          amount: 1000,
+        },
+        {
+          id: 10,
+          date: "2019-12-16",
+          description: "Tickets, Flatiron Multiplex Cinemas",
+          category: "Entertainment",
+          amount: -24,
+        },
+        {
+          id: 11,
+          date: "2019-12-16",
+          description: "MTA Vending Machine: MetroCard",
+          category: "Transportation",
+          amount: -116.39,
+        },
+        {
+          id: 12,
+          date: "2019-12-17",
+          description: "Venmo, Pay Roommate for Rent",
+          category: "Housing",
+          amount: -975,
+        },
+        {
+          date: "2022-07-09",
+          description: "Office lunch",
+          category: "Food",
+          amount: "2000",
+          id: 13,
+        },
+        {
+          date: "2022-07-09",
+          description: "Office lunch Wednesday",
+          category: "Food",
+          amount: "3000",
+          id: 14,
+        },
+      ],
+    };
+    setTransactions(sampleData.transactions);
   }, []);
 
   const addTransaction = (newTransaction) => {
-    setTransactions([...transactions, newTransaction]);
+    const newTransactionWithId = {
+      ...newTransaction,
+      id:
+        transactions.length > 0
+          ? Math.max(...transactions.map((t) => t.id)) + 1
+          : 1,
+    };
+
+    setTransactions([...transactions, newTransactionWithId]);
   };
 
   const deleteTransaction = (id) => {
@@ -56,7 +165,6 @@ function Transactions() {
           const amount = parseFloat(e.target.amount.value);
 
           const newTransaction = {
-            id: transactions.length + 1,
             date: new Date().toISOString().split("T")[0],
             description: description,
             category: category,
